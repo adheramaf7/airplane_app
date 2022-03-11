427618de580f84a794170cd42e5453991c03ad40
Adhe Rama F <adheramaf7@gmail.com>
2022-03-11 10:13:13 +0700

    splashpage & getstartedpage

---
+++ lib/ui/pages/get_started_page.dart
@@ -0,0 +1,62 @@
+import 'package:airplane_app/shared/theme.dart';
+import 'package:flutter/material.dart';
+
+class GetStartedPage extends StatelessWidget {
+  const GetStartedPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(children: [
+        Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage('assets/image_get_started.png'),
+            ),
+          ),
+        ),
+        Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text(
+                'Fly like a Bird',
+                style: whiteTextStyle.copyWith(fontSize: 32, fontWeight: bold),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                'Explore new world with us and let\nyourself get an amazing experiences',
+                style: whiteTextStyle.copyWith(fontSize: 16, fontWeight: light),
+                textAlign: TextAlign.center,
+              ),
+              Container(
+                width: 220,
+                height: 55,
+                margin: const EdgeInsets.only(top: 50, bottom: 80),
+                child: TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    backgroundColor: kPrimaryColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(17),
+                    ),
+                  ),
+                  child: Text(
+                    'GetStarted',
+                    style: whiteTextStyle.copyWith(
+                        fontSize: 18, fontWeight: medium),
+                  ),
+                ),
+              )
+            ],
+          ),
+        )
+      ]),
+    );
+  }
+}
